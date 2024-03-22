1efe159783430e8b6da3e77c1532bac63b3debc5
mwunnam <mwunnam@gmail.com>
2024-03-22 19:18:54 +0000

    done with task 2

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# manifest.pp
+# Using the exec resource to kill the "killmenow" process
+exec { 'kill_killmenow_process':
+  command     => 'pkill -f killmenow',
+  path        => '/bin:/usr/bin',
+  refreshonly => true,
+}
