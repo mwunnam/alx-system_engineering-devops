bffb0ac8b9ca62184f106545e0f4e9e2ccd9fcaa
mwunnam <mwunnam@gmail.com>
2024-03-23 06:02:43 +0000

    correction

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/pup
 # Using the exec resource to kill the "killmenow" process
-exec { 'kill_killmenow_process':
-  command     => 'pkill -f killmenow',
-  path        => '/bin:/usr/bin',
-  refreshonly => true
+exec { 'pkill':
+  command     => 'pkill killmenow',
+  provider    => 'shell'
 }
