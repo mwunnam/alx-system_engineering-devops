82d072ae8e634b43e5b56022283b459121c1541c
mwunnam <mwunnam@gmail.com>
2024-03-23 06:16:37 +0000

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 # This will install flask with version 2.1.0
-package {'Flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-package ('Werkzeug':
+package ( 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
 }
