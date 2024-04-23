7566f415603ac9f23f17c8e4fa5cae8bb8ccfe25
mwunnam <mwunnam@gmail.com>
2024-04-23 08:26:11 +0000

    done with task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,22 +11,32 @@ def employee_todo_progress(employee_id):
     Function that will get information about and employee's
     todo progress
     """
+    """Setting up urls"""
     base_url = 'https://jsonplaceholder.typicode.com'
     user_url = f'{base_url}/users/{employee_id}'
     todos_url = f'{base_url}/todos?userId={employee_id}'
 
+    """Getting user data"""
     user_response = requests.get(user_url)
     user_data = user_response.json()
-    empoyee_name = user_data['name']
+    employee_name = user_data['name']
 
-    total_task = len(todo_data)
+    """Getting todo data"""
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+    total_tasks = len(todos_data)
+
+    """Calculating task completed"""
     completed_tasks = 0
     for todo in todos_data:
         if todo['completed']:
             completed_tasks += 1
 
-    print(f'Employee {employee_name} is done with \
-          tasks({completed_tasks}/{total_tasks}): ')
+    progress_message = (
+            f'Employee {employee_name} is done with tasks '
+            f'({completed_tasks}/{total_tasks}): '
+    )
+    print(progress_message)
     for todo in todos_data:
         if todo['completed']:
             print(f"\t{todo['title']}")
